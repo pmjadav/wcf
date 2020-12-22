6e8152e929c5446f3b4803fdcad3e649481e8e25
Prashant Jadav <pmjadav@gmail.com>
2020-12-22 18:39:32 +0530

    Added Web App Client for Calculator Service.

---
+++ CalcWebClient/CalcForm.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalcForm.aspx.cs" Inherits="CalcWebClient.CalcForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <% Response.Write("Calculator Client for WCF Service"); %>
+        </div>
+        <br />
+        <asp:TextBox ID="txtOp1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtOp2" runat="server"></asp:TextBox>
+        <asp:Label ID="lblResult" runat="server" Text="0.00"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+        <asp:Button ID="btnMul" runat="server" OnClick="btnMul_Click" Text="Multiply" />
+    </form>
+</body>
+</html>
---
+++ CalcWebClient/CalcForm.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalcWebClient
+{
+    public partial class CalcForm : System.Web.UI.Page
+    {
+        CalcServRef.CalculatorClient client = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            client = new CalcServRef.CalculatorClient("BasicHttpBinding_ICalculator");
+        }     
+
+        protected void btnMul_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = client.Mul(double.Parse(txtOp1.Text), 
+                                        double.Parse(txtOp2.Text)).ToString();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = client.Add(double.Parse(txtOp1.Text), 
+                                        double.Parse(txtOp2.Text)).ToString();
+        }
+    }
+}
